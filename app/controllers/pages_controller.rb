a8f0467376c339a3625b2dbe8ea816f5ff5244eb
tainan bogo <tainanbogo@gmail.com>
2023-09-04 08:21:57 -0300

    new pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def who
+    
+  end
+
+  def associate
+  end
+
+  def projects
+  end
+
+  def partners
+  end
+
+  def contact
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root "pages#home";
+  get "pages/who", to: 'pages#who';
+  get "pages/associate", to: 'pages#associate';
+  get "pages/projects", to: 'pages#projects';
+  get "pages/partners", to: 'pages#partners';
+  get "pages/contact", to: 'pages#contact';
+  
 end
